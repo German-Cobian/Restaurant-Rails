42000c496c5f51f7d6026a0970af670ae9b0a7e6
German Renato Cobian <germancobian@hotmail.com>
2024-01-16 15:31:06 -0800

    Create Order-item model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_16_232017) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_16_232509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "order_items", force: :cascade do |t|
+    t.decimal "unit_price"
+    t.integer "quantity"
+    t.decimal "total"
+    t.bigint "product_id", null: false
+    t.bigint "order_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.decimal "subtotal"
@@ -38,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_16_232017) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
 end
