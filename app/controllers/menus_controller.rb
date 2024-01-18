233358cc4e693dd7f7da0fa757bcf3caa4abf49a
German Renato Cobian <germancobian@hotmail.com>
2024-01-17 18:03:09 -0800

    Implement functionality for Search + Implement merger of Products with Category in products_controller and views/menus/products

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,7 +1,26 @@
 class MenusController < ApplicationController
+  before_action :set_categories
 
   def index
     @page = 'menu'
     @products = Product.all
+  end
+
+  def search
+    query = params[:search]
+
+    results = Products.where('name LIKE ?', "%#{query}%" )
+    if params[:filter] == 'Select Filter'
+      @products = results
+    else
+      symbol = params[:filter].gsub(/ /, '_').downcase!.to_sym
+      @products = results.where(symbol => true)
+    end
+  end
+
+  private
+
+  def set_categories
+    @categories = Category.all.where(display: true)
   end
 end
